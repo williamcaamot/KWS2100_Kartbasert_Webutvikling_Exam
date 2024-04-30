20e61bbf05cd7d407ef6a8c1d96b8ed6a4a4bdf3
williamdev <william-ca@live.no>
2024-04-30 09:31:11 +0200

    Add eiendom/teig layer

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -17,7 +17,7 @@ import useLocalStorageState from "use-local-storage-state";
 import ResetIcon from "./icons/ResetIcon";
 import { LoadingSpinner } from "./LoadingSpinner";
 import { MapContext } from "../modules/map/mapContext";
-import {EiendomCheckbox} from "../modules/layers/eiendommer/EiendomLayerCheckbox";
+import { EiendomCheckbox } from "../modules/layers/eiendommer/EiendomLayerCheckbox";
 
 export default function Sidebar() {
   const { loadingQueue } = useContext(MapContext);
@@ -253,7 +253,7 @@ function DataLayerContent() {
       <PopulationLayer />
       <MatbutikkerCheckbox />
       <TrainLayer />
-      <EiendomCheckbox/>
+      <EiendomCheckbox />
     </div>
   );
 }
