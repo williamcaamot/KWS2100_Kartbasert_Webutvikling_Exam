f7db03bbd8acf1ab7cd24a1774fdc96f5830d8da
williamdev <william-ca@live.no>
2024-04-30 07:48:12 +0200

    remade icons to be better

--- src/ui/icons/ResetIcon.tsx
+++ src/ui/icons/ResetIcon.tsx
@@ -21,11 +21,11 @@ const ResetIcon: React.FC<ResetIconProps> = ({
       strokeLinejoin="round"
     >
       <g
-          fill="none"
-          fillRule="evenodd"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path d="M3.578 6.487A8 8 0 112.5 10.5M7.5 6.5h-4v-4" />
       </g>
