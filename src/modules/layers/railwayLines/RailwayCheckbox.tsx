18f244cd8ee1c13beae60b436e2a1bfcff08a8b4
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-01 12:59:27 +0200

    Changed some names to make everything similar

--- src/modules/layers/railwayLines/RailwayCheckbox.tsx
+++ src/modules/layers/railwayLines/RailwayCheckbox.tsx
@@ -9,26 +9,26 @@ import { FeatureLike } from "ol/Feature";
 import Switch from "../../../ui/switch";
 
 import {
-  JernbanelinjerFeature,
-  activeJernbanelinjerStyle,
-  jernbaneLinjerStyle,
-} from "./jernbaneLinjerFeature";
+  RailwayFeature,
+  activeRailwayStyle,
+  railwayStyle,
+} from "./RailwayFeature";
 
-const jernbaneLinjerLayer = new VectorLayer({
+const railwayLayer = new VectorLayer({
   source: new VectorSource({
     url: "/kws2100-exam-williamcaamot/layers/jernbanelinjer-N50.json",
     format: new GeoJSON(),
   }),
-  style: jernbaneLinjerStyle,
-  className: "jernbanelinjer",
+  style: railwayStyle,
+  className: "railway",
 });
 
-export function JernbaneLinjerLayerCheckbox() {
+export function RailwayLayerCheckbox() {
   const { map } = useContext(MapContext);
 
   const [checked, setChecked] = useState(false);
 
-  const [activeFeature, setActiveFeature] = useState<JernbanelinjerFeature>();
+  const [activeFeature, setActiveFeature] = useState<RailwayFeature>();
 
   function handlePointerMove(e: MapBrowserEvent<MouseEvent>) {
     const resolution = map.getView().getResolution();
@@ -38,17 +38,17 @@ export function JernbaneLinjerLayerCheckbox() {
     const features: FeatureLike[] = [];
     map.forEachFeatureAtPixel(e.pixel, (f) => features.push(f), {
       hitTolerance: 5,
-      layerFilter: (l) => l === jernbaneLinjerLayer,
+      layerFilter: (l) => l === railwayLayer,
     });
     if (features.length === 1) {
-      setActiveFeature(features[0] as JernbanelinjerFeature);
+      setActiveFeature(features[0] as RailwayFeature);
     } else {
       setActiveFeature(undefined);
     }
   }
 
   useEffect(() => {
-    activeFeature?.setStyle(activeJernbanelinjerStyle);
+    activeFeature?.setStyle(activeRailwayStyle);
     return () => activeFeature?.setStyle(undefined);
   }, [activeFeature]);
 
@@ -59,11 +59,11 @@ export function JernbaneLinjerLayerCheckbox() {
     return () => map?.un("pointermove", handlePointerMove);
   }, [checked]);
 
-  useLayer(jernbaneLinjerLayer, checked);
+  useLayer(railwayLayer, checked);
 
   return (
     <div className={"flex w-full justify-around p-1"}>
-      <p>Jernbanelinjer N50</p>
+      <p>Railwaylines N50</p>
       <div className={"flex-1"}></div>
       <Switch checked={checked} onChange={setChecked} />
     </div>
---
+++ src/modules/layers/railwayLines/RailwayFeature.tsx
@@ -0,0 +1,40 @@
+import { Feature } from "ol";
+import { Point } from "ol/geom";
+import { FeatureLike } from "ol/Feature";
+import { Fill, Stroke, Style, Text } from "ol/style";
+
+export type RailwayFeature = {
+  getProperties(): railwayProperties;
+} & Feature<Point>;
+
+export interface railwayProperties {
+  sporantall: string;
+  medium: string;
+}
+
+export const railwayStyle = (feature: FeatureLike) => {
+  const railway = feature.getProperties() as railwayProperties;
+  return new Style({
+    stroke: new Stroke({
+      color: railway.sporantall === "E" ? "#0F172A" : "#0D9488",
+      width: 3,
+    }),
+  });
+};
+
+export const activeRailwayStyle = (feature: FeatureLike) => {
+  const railway = feature.getProperties() as railwayProperties;
+  return new Style({
+    stroke: new Stroke({
+      color: railway.sporantall === "E" ? "#94A3B8" : "#0D9488",
+      width: 3,
+    }),
+    text: new Text({
+      text: railway.sporantall + " " + railway.medium,
+      font: "bold 14px sans-serif",
+      stroke: new Stroke({ color: "white", width: 2 }),
+      fill: new Fill({ color: "black" }),
+      offsetY: -10,
+    }),
+  });
+};
