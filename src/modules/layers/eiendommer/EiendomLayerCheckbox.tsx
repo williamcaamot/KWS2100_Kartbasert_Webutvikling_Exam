9d253ee42eff988909ac4d7e296935c515dc3ff0
williamdev <william-ca@live.no>
2024-05-01 10:29:57 +0200

    Changed styling for eiendommer and added onclick for eiendommer

--- src/modules/layers/eiendommer/EiendomLayerCheckbox.tsx
+++ src/modules/layers/eiendommer/EiendomLayerCheckbox.tsx
@@ -1,34 +1,37 @@
-import React, {useContext, useEffect, useState} from "react";
-import {MapContext} from "../../map/mapContext";
+import React, { useContext, useEffect, useState } from "react";
+import { MapContext } from "../../map/mapContext";
 import Switch from "../../../ui/switch";
-import {EiendomFeature, EiendomLayer} from "./EiendomLayer";
-import {MapBrowserEvent, Overlay} from "ol";
-import {useLayer} from "../../map/useLayer";
+import { EiendomFeature, EiendomLayer } from "./EiendomLayer";
+import { MapBrowserEvent, Overlay } from "ol";
+import { useLayer } from "../../map/useLayer";
 import useLocalStorageState from "use-local-storage-state";
-import {TrainFeature} from "../trains/TrainFeature";
-import {AdresseFeature} from "../adresser/AdresseLayer";
+import { TrainFeature } from "../trains/TrainFeature";
+import { AdresseFeature } from "../adresser/AdresseLayer";
 
 export function EiendomCheckbox() {
-    const {map} = useContext(MapContext);
-    const [checked, setChecked] = useLocalStorageState("eiendom-layer-checked", {
-        defaultValue: false,
-    });
+  const { map } = useContext(MapContext);
+  const [checked, setChecked] = useLocalStorageState("eiendom-layer-checked", {
+    defaultValue: false,
+  });
 
-    function handleClick(e: MapBrowserEvent<MouseEvent>) {
-        const resolution = map.getView().getResolution();
-        if (!resolution || resolution > 1000) {
-            return;
-        }
+  function handleClick(e: MapBrowserEvent<MouseEvent>) {
+    const resolution = map.getView().getResolution();
+    if (!resolution || resolution > 1000) {
+      return;
+    }
 
-        var featuresAtCoordinate = map.forEachFeatureAtPixel(e.pixel, function (feature) {
-            return feature as EiendomFeature;
-        });
+    var featuresAtCoordinate = map.forEachFeatureAtPixel(
+      e.pixel,
+      function (feature) {
+        return feature as EiendomFeature;
+      },
+    );
 
-        // Show the popup here
-        const coordinate = e.coordinate;
-        popup.setPosition(coordinate);
-        if (featuresAtCoordinate?.getProperties().matrikkelenhetid) {
-            popupElement.innerHTML = `
+    // Show the popup here
+    const coordinate = e.coordinate;
+    popup.setPosition(coordinate);
+    if (featuresAtCoordinate?.getProperties().matrikkelenhetid) {
+      popupElement.innerHTML = `
           <span>
           <ul>
             <li><span class="font-bold">MatrikkelenhetID:</span> ${featuresAtCoordinate?.getProperties().matrikkelenhetid}</li>
@@ -40,45 +43,42 @@ export function EiendomCheckbox() {
             </ul>
           </span>
         `;
-            popupElement.style.display = "block";
-        }
-
+      popupElement.style.display = "block";
     }
+  }
 
-    const popupElement = document.createElement("div");
-    popupElement.style.backgroundColor = "white";
-    popupElement.style.padding = "10px";
-    popupElement.style.borderRadius = "5px";
-    popupElement.style.border = "1px solid black";
+  const popupElement = document.createElement("div");
+  popupElement.style.backgroundColor = "white";
+  popupElement.style.padding = "10px";
+  popupElement.style.borderRadius = "5px";
+  popupElement.style.border = "1px solid black";
+  popupElement.style.display = "none";
+  document.body.appendChild(popupElement);
+
+  const popup = new Overlay({
+    element: popupElement,
+  });
+  map.addOverlay(popup);
+
+  map.on("pointermove", function () {
     popupElement.style.display = "none";
-    document.body.appendChild(popupElement);
+  });
 
-    const popup = new Overlay({
-        element: popupElement,
-    });
-    map.addOverlay(popup);
+  useLayer(EiendomLayer, checked);
 
+  // Effect for adding and removing onclick
+  useEffect(() => {
+    if (checked) {
+      map?.on("click", handleClick);
+    }
+    return () => map?.un("click", handleClick);
+  }, [checked]);
 
-    map.on("pointermove", function () {
-        popupElement.style.display = "none";
-    });
-
-
-    useLayer(EiendomLayer, checked);
-
-// Effect for adding and removing onclick
-    useEffect(() => {
-        if (checked) {
-            map?.on("click", handleClick);
-        }
-        return () => map?.un("click", handleClick);
-    }, [checked]);
-
-    return (
-        <div className={"flex w-full justify-around p-1"}>
-            <p>Vis eiendommer</p>
-            <div className={"flex-1"}></div>
-            <Switch checked={checked} onChange={setChecked}/>
-        </div>
-    );
+  return (
+    <div className={"flex w-full justify-around p-1"}>
+      <p>Vis eiendommer</p>
+      <div className={"flex-1"}></div>
+      <Switch checked={checked} onChange={setChecked} />
+    </div>
+  );
 }
